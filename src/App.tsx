d7ca34f589a21c8d9b8d12213dba9dbbf1b11ed6
realmann <sappiahjob085@gmail.com>
2026-02-10 19:41:14 +0100

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback } from "react";
 import FloatingHearts from "./components/FloatingHearts";
 import RunawayButton from "./components/RunawayButton";
 import EasterEggs from "./components/EasterEggs";
